f3108194578ca6e7e1251714d89611707908de43
groundnuty <groundnuty@gmail.com>
2020-12-04 13:59:41 +0100

    cli optimization now handles } in examples

--- split.pl
+++ split.pl
@@ -7,20 +7,24 @@ use strict;
 
 my $in_function = "";
 my $path = $ARGV[0] ;
+my $quotes_count = 0 ;
 
 while(<>)
 {
   my ($start) = m{^(\w+)\(\)\s*\{$};
   my ($end) = m{^\}$};
 
-  if (defined $start) {
+  my @quotes = $_ =~ /"/g ;
+  $quotes_count += scalar @quotes ;
+
+ if (defined $start) {
     $in_function = $1 ;
     open FH, "> $in_function.file" or die $in_function;
     print FH "_",$in_function,"() {\n" ;
     print $in_function,"() {\n" ;
     print "    . \"$path/$in_function.file\"\n" ;
     print "    _$in_function \"\$@\"\n" ;
-  } elsif (defined $end) {
+  } elsif ( ($quotes_count % 2) == 1 and defined $end){
     $in_function = "" ;
     print FH "}\n" ;
     print "}\n" ;
